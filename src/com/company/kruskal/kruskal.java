bff66e13861cebb0e266918aeb3bc9cd5a0f41a6
DESKTOP-VSDKPQV\tmdgh <tmdghks7836@naver.com>
2022-01-19 23:08:19 +0900

    최소 신장 트리 구현 (완벽하게 이해되지 않음)

--- src/com/company/kruskal/kruskal.java
+++ src/com/company/kruskal/kruskal.java
@@ -13,6 +13,8 @@ public class kruskal {
 
     List<KruskalEdge> results = new ArrayList<>();
     Map<Character, Character> parentMap = new HashMap();
+    Map<Character, Integer> rank = new HashMap();
+
 
     public void start(List<KruskalEdge> edges, char[] vertices) {
 
@@ -21,20 +23,41 @@ public class kruskal {
 
         edges.forEach(edge -> {
 
-//            findParent(edge);
+            if(findParent(edge.vertex) != findParent(edge.next)){
 
+                union(edge.vertex, edge.next);
+                results.add(edge);
+            }
         });
-        //union
-//        if()
 
-        //add
+        results.forEach(kruskalEdge -> System.out.println(kruskalEdge.vertex + " to "+ kruskalEdge.next + " = " + kruskalEdge.weight));
     }
 
+    private void union(char parentVertex, char nextParentVertex) {
+
+        char root1 = findParent(parentVertex);
+        char root2 = findParent(nextParentVertex);
+
+        if (rank.get(root1) > rank.get(root2)) {
+
+            parentMap.put(root2, root1);
+        } else {
+            parentMap.put(root1, root2);
+            if (rank.get(root1) == rank.get(root2)) {
+                int newRank = parentMap.get(root1) + 1;
+                rank.put(root1, newRank);
+            }
+        }
+    }
+
+    //path compression
     private char findParent(char vertex) {
 
         //만약 부모가 자신이 아니라면
         if (!parentMap.get(vertex).equals(vertex)) {
-            char findVertex = findParent(parentMap.get(vertex));
+            char parent = findParent(parentMap.get(vertex));
+
+            parentMap.put(vertex, parent);
         }
 
         return parentMap.get(vertex);
@@ -44,6 +67,7 @@ public class kruskal {
 
         for (char vertex : vertices) {
             parentMap.put(vertex, vertex);
+            rank.put(vertex, 0);
         }
     }
 
