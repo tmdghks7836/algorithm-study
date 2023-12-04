dc7663972351d33055fc499c7ca9ebbbeecf7510
tmdghks0021 <tmdghks7836@naver.com>
2023-12-04 17:49:12 +0900

    merge

---
+++ src/com/company/대기업유제/자료구조활용/문자열압축해제.java
@@ -0,0 +1,18 @@
+package com.company.대기업유제.자료구조활용;
+
+class 문자열압축해제 {
+	public String solution(String s){
+		String answer = "";
+		
+		return answer;
+	}
+
+	public static void main(String[] args){
+		문자열압축해제 T = new 문자열압축해제();
+		System.out.println(T.solution("3(a2(b))ef"));
+		System.out.println(T.solution("2(ab)k3(bc)"));
+		System.out.println(T.solution("2(ab3((cd)))"));
+		System.out.println(T.solution("2(2(ab)3(2(ac)))"));
+		System.out.println(T.solution("3(ab2(sg))"));
+	}
+}
